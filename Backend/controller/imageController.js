f1784cdf5f3823fa8eaa449e6b327219a8195cb1
Ali Khan <88495045+alik95997@users.noreply.github.com>
2025-07-23 20:06:05 +0500

    Update imageController.js

---
+++ Backend/controller/imageController.js
@@ -0,0 +1,23 @@
+import {cloudinaryUploader} from "../config/cloudinary.js";
+import fs from "fs";
+
+export const uploadImageController = async (req,res)=>{
+  try{
+    console.log(req.files[0].path);
+    const filepath = req.files[0].path;
+    const imageRes = await cloudinaryUploader.upload(filepath);
+    fs.unlink(filepath,(err,res)=>{
+      
+    })
+    res.json({
+      message:"Image is uploaded successfully",
+      status:true;
+    })
+  }
+  catch(error){
+    res.json({
+      message:error.message,
+      status:false;
+    })
+  }
+}
