7030153dd46481a390e85fafecd5de91a0a7527e
Ali Khan <88495045+alik95997@users.noreply.github.com>
2025-08-01 18:10:56 +0500

    Update restaurantController.js

--- Backend/controller/restaurantController.js
+++ Backend/controller/restaurantController.js
@@ -30,19 +30,75 @@ try{
   console.log("userId", userId);
   const data = await RestaurantModel.find({createdBy:userId, isDeleted:false});
   res.json({
-    message "Restaurant fetched successfuly!",
-    status:true,
-    data:data
+    message "Restaurant fetched successfully!",
+    status: true,
+    data: data
   })
 }
 catch(error){
   res.json({
-    message:error.message || "something went wrong",
-    status:false
+    message: error.message || "something went wrong",
+    status: false
   })
 }
 }
 
-export const deleteVendorRes = async (req,res)=>{
-  const 
+export const deleteVendorRes = async (req, res)=>{
+  const params = req.params;
+  const updatedObj = {
+    isDeleted: true
+  }
+  await RestaurantModel.findByIdAndUpdate(params.id, updatedObj);
+  res.status(200).json({
+    message: "Deleted Successfully",
+    status: true
+  })
+}
+
+export const updateVendorRestaurant = async (req, res) => {
+
+try{
+  const body = req.body;
+  const {id} = req.params;
+  const data = await RestaurantModel.findByIdAndUpdate(id, body, {new : true });
+  res.status(200).json({
+    message: "Updated successfully",
+    status: true,
+    data: data
+  })    
+}
+  catch(error){
+    res.status(400).json(
+      message: error.message || "Something went wrong",
+      status: false
+    )
+  }                            
+}
+
+export const vendorRestaurantStatus = async (req, res) =>{
+ try{
+   const body = req.body;
+   const { id } = req.params;
+   const restaurantData = await RestaurantModel.findById(id);
+   if (!restaurantData.isApproved){
+     return res.status(400).json(
+       message: "Your Restaurant is not approved, please wait for admin approval",
+       status: false
+     )
+   }
+   const updatedObj = {
+     isOpen : body.isOpen
+   }
+   await RestaurantModel.findByIdAndUpdate(id, updatedObj, {new: true});
+   return res.status(200).json({
+     message: "Restaurant status is updated",
+     status: true
+   })
+ }
+  catch(error){
+    res.status(400).json(
+      message: error.message || "Something went wrong",
+      status: false
+    )
+  }
 }
